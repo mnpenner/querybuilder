be36ccaf7832962f220fb090f6896a51a316576f
Mark Bayazit <mbayazit@nucleuslabs.com>
2014-10-14 16:22:27 -0700

    more flags

--- src/SelectExpression.php
+++ src/SelectExpression.php
@@ -56,12 +56,24 @@ SELECT
 TODO: make immutable
  */
 class SelectExpression {
-    /** @var bool Remove duplicate rows from result set */
-    protected $distinct = false;
+    /** @var bool|null Remove duplicate rows from result set */
+    protected $distinct = null;
     /** @var bool Give the select statement higher priority than a statement that updates a table */
     protected $highPriority = false;
     /** @var int Statement execution timeout in ms */
     protected $maxStatementTime = 0;
+    /** @var bool Force optimizer to join the tables in the order in which they are listed in the FROM clause */
+    protected $straightJoin = false;
+    /** @var bool */
+    protected $smallResult = false;
+    /** @var bool  */
+    protected $bigResult = false;
+    /** @var bool  */
+    protected $bufferResult = false;
+    /** @var int */
+    protected $cache = SqlCache::DEFAULT_CACHE;
+    /** @var bool */
+    protected $calcFoundRows = false;
 
     /**
      * ALL (the default) specifies that all matching rows should be returned, including duplicates.
@@ -102,8 +114,101 @@ class SelectExpression {
      * This option was added in MySQL 5.7.4.
      *
      * @param int $ms Milliseconds
+     * @return $this
      */
     public function maxStatementTime($ms) {
         $this->maxStatementTime = $ms;
+        return $this;
+    }
+
+    /**
+     *  STRAIGHT_JOIN forces the optimizer to join the tables in the order in which they are listed in the FROM clause. You can use this to speed up a query if the optimizer joins the tables in nonoptimal order. STRAIGHT_JOIN also can be used in the table_references list.  See Section 13.2.9.2, “JOIN Syntax”.
+     *
+     * STRAIGHT_JOIN does not apply to any table that the optimizer treats as a const or system table. Such a table produces a single row, is read during the optimization phase of query execution, and references to its columns are replaced with the appropriate column values before query execution proceeds. These tables will appear first in the query plan displayed by EXPLAIN. See Section 8.9.1, “Optimizing Queries with EXPLAIN”. This exception may not apply to const or system tables that are used on the NULL-complemented side of an outer join (that is, the right-side table of a LEFT JOIN or the left-side table of a RIGHT JOIN.
+     *
+     * @return $this
+     */
+    public function straightJoin() {
+        return $this;
+    }
+
+    /**
+     * MySQL directly uses disk-based temporary tables if needed, and prefers sorting to using a temporary table with a key on the GROUP BY elements.
+     *
+     * @return $this
+     */
+    public function bigResult() {
+        $this->bigResult = true;
+        return $this;
+    }
+
+    /**
+     * MySQL uses fast temporary tables to store the resulting table instead of using sorting. This should not normally be needed.
+     *
+     * @return $this
+     */
+    public function smallResult() {
+        $this->smallResult = true;
+        return $this;
+    }
+
+    /**
+     * SQL_BUFFER_RESULT forces the result to be put into a temporary table. This helps MySQL free the table locks early and helps in cases where it takes a long time to send the result set to the client. This option can be used only for top-level SELECT statements, not for subqueries or following UNION.
+     *
+     * @return $this
+     */
+    public function bufferResult() {
+        $this->bufferResult = true;
+        return $this;
+    }
+
+    /**
+     * SQL_CACHE tells MySQL to store the result in the query cache if it is cacheable and the value of the query_cache_type system variable is 2 or DEMAND.
+     *
+     * For a cacheable query, SQL_CACHE applies if it appears in the first SELECT of a view referred to by the query.
+     *
+     * @return $this
+     */
+    public function cache() {
+        $this->cache = SqlCache::CACHE;
+        return $this;
+    }
+
+    /**
+     * With SQL_NO_CACHE, the server does not use the query cache. It neither checks the query cache to see whether the result is already cached, nor does it cache the query result.
+     *
+     * For views, SQL_NO_CACHE applies if it appears in any SELECT in the query.
+     *
+     * @return $this
+     */
+    public function noCache() {
+        $this->cache = SqlCache::NO_CACHE;
+        return $this;
+    }
+
+    /**
+     * Tells MySQL to calculate how many rows there would be in the result set, disregarding any LIMIT clause. The number of rows can then be retrieved with SELECT FOUND_ROWS().
+     *
+     * @return $this
+     */
+    public function calcFoundRows() {
+        $this->calcFoundRows = true;
+        return $this;
+    }
+
+    public function toSql() {
+        $sb = ['SELECT'];
+        if($this->distinct === true) $sb[] = 'DISTINCT';
+        elseif($this->distinct === false) $sb[] = 'ALL';
+        if($this->maxStatementTime !== 0) $sb[] = 'MAX_STATEMENT_TIME = '.$this->maxStatementTime;
+        if($this->straightJoin) $sb[] = 'STRAIGHT_JOIN';
+        if($this->smallResult) $sb[] = 'SQL_SMALL_RESULT';
+        if($this->bigResult) $sb[] = 'SQL_BIG_RESULT';
+        if($this->bufferResult) $sb[] = 'SQL_BUFFER_RESULT';
+        if($this->cache === SqlCache::CACHE) $sb[] = 'SQL_CACHE';
+        elseif($this->cache === SqlCache::NO_CACHE) $sb[] = 'SQL_NO_CACHE';
+        if($this->calcFoundRows) $sb[] = 'SQL_CALC_FOUND_ROWS';
+
+        return implode(' ',$sb);
     }
 }
---
+++ src/SqlCache.php
@@ -0,0 +1,8 @@
+<?php
+namespace QueryBuilder;
+
+class SqlCache {
+    const DEFAULT_CACHE = 0;
+    const CACHE = 1;
+    const NO_CACHE = 2;
+}
